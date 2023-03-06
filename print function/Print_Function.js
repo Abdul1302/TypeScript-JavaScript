8d9eba74e4bbfe04257cf4e197700a80cf094c7c
Abdul1302 <106179686+Abdul1302@users.noreply.github.com>
2023-03-06 18:12:13 +0500

    Add files via upload

---
+++ print function/Print_Function.js
@@ -0,0 +1,16 @@
+//print function
+console.log('I am Abdul Rehman');
+
+//print Variable
+let my_name='Abdul Rehman';
+console.log(my_name);
+
+//String Concatination
+let name_1='Abdul';
+let name_2='Rehman';
+console.log(name_1+' '+name_2);
+//String Formatting
+
+var names = 'Abdullah';
+var age = '21';
+console.log("My Name is ".concat(names, " and I am ").concat(age, " years old"));
